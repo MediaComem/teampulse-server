1fd8c8fddaf9484c4ce2b07ac8e5205df34e2d6d
Oreonax <esok@mailoo.org>
2017-05-17 17:33:56 +0200

    Wrap in object function + write and read in separate json files

--- app.js
+++ app.js
@@ -1,124 +1,187 @@
+var fs = require('fs');
+var dotenv = require('dotenv').config()
 var express = require('express')
 var app = express()
 var cors = require('cors')
 var Twitter = require('twitter');
 var graph = require('fbgraph');
 var fetch = require('node-fetch');
-var dotenv = require('dotenv').config()
+
 var Flickr = require("flickrapi");
 
 var corsOptions = {
-  origin: 'https://mediacomem.github.io',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
+	origin: 'https://mediacomem.github.io',
+	optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
 }
 
-/*****************
-  INSTAGRAM
-******************/
+var dataPath = "data/";
 
-var instaPostsId = []
-fetch('https://api.instagram.com/v1/users/self/media/liked?access_token='+process.env.INSTAGRAM_ACCESS_TOKEN)
-  .then(function(response) {
-    return response.json()
-  })
-  .then(function(body){
-    for (i = 0; i < body.data.length; i++) { 
-	    instaPostsId.push({id:body.data[i].id,link:body.data[i].link})
-		}
-  });
+var socialFetch = {
 
-/*****************
-  FLICKR
-******************/
-var flickrOptions = {
-  api_key: process.env.FLICKR_API_KEY,
-  secret: process.env.FLICKR_SECRET
-};
+	init: function () {
+		this.instagram();
+		this.facebook();
+		this.flickr();
+	},
 
-var flickrPicsId = []
-Flickr.tokenOnly(flickrOptions, function(error, flickr) {
-  flickr.photosets.getPhotos({
-	  photoset_id: "72157661572108305",
-	  user_id: "135987724@N05",
-	  page: 1,
-	  per_page: 500
-	}, function(err, result) {
-		if(err) throw err;
-	  for (i = 0; i < result.photoset.photo.length; i++) {
-	  	flickr.photos.getInfo({
-			  photo_id: result.photoset.photo[i].id,
-			}, function(err, res) {
-				if(err) throw err;
-				// console.log(res.photo)
-				var sourceUrl = "https://farm"+res.photo.farm+".staticflickr.com/"+res.photo.server+"/"+res.photo.id+"_"+res.photo.secret+"_z.jpg"
-				// console.log(sourceUrl)
-			  flickrPicsId.push(sourceUrl)
+	/*****************
+		INSTAGRAM
+	******************/
+
+	instagram: function () {
+		fetch('https://api.instagram.com/v1/users/self/media/liked?access_token=' + process.env.INSTAGRAM_ACCESS_TOKEN)
+			.then(function (res) {
+				return res.json()
+			})
+			.then(function (body) {
+				return body.data.map(function (d) {
+					return {
+						id: d.id,
+						link: d.link
+					}
+				});
+			})
+			.then(function (posts) {
+				tools.writeJson("instagram.json", posts);
+			})
+			.catch(function (err) {
+				console.log(err);
 			});
-			flickr.photos.getSizes({
-			  photo_id: result.photoset.photo[i].id,
-			}, function(err, res) {
-				if(err) throw err;
-			});
-		}
-	});
-});
+	},
 
-/*****************
+	/*****************
+  	FLICKR
+	******************/
+	flickrOptions: {
+		api_key: process.env.FLICKR_API_KEY,
+		secret: process.env.FLICKR_SECRET
+	},
+	flickr: function () {
+		Flickr.tokenOnly(this.flickrOptions, function (error, flickr) {
+			flickr.photosets.getPhotos({
+				photoset_id: "72157661572108305",
+				user_id: "135987724@N05",
+				page: 1,
+				per_page: 500
+			}, function (err, result) {
+				if (err) throw err;
+
+				var photos = result.photoset.photo.map(function (d) {
+					return {
+						url: "https://farm" + d.farm + ".staticflickr.com/" + d.server + "/" + d.id + "_" + d.secret + "_z.jpg"
+					}
+				});
+
+				tools.writeJson("flickr.json", photos);
+			});
+		});
+	},
+
+	/*****************
   FACEBOOK
-******************/
-graph.setAccessToken(process.env.FACEBOOK_ACCESS_TOKEN);
+	******************/
 
-var fbPostsId = []
-var graphObject = graph
-  .get("teampulse.ch/feed?limit=5", function(err, res) {
-    if(err) throw err;
-	  // console.log(res);  // The favorites.
-	  for (i = 0; i < res.data.length; i++) { 
-	  	var str = res.data[i].id;
-			str = str.split("_").pop();
-	    fbPostsId.push(str)
-		}
-  });
+	facebook: function () {
+		graph.setAccessToken(process.env.FACEBOOK_ACCESS_TOKEN);
+
+		graph.get("teampulse.ch/feed?limit=5", function (err, res) {
+			if (err) throw err;
+
+			var posts = res.data.map(function (d) {
+				return {
+					id: d.id.split("_").pop()
+				}
+			});
+
+			tools.writeJson("facebook.json", posts);
+		});
+	}
+}
 
 /*****************
   TEAMPULSE API
 ******************/
-var fakedata = {"contestant":"CYCLIST_002","latitude":46.764446,"longitude":6.646111,"numberMinutes":30,"avgSpeed":8.780640602111816,"avgCadence":40.0,"avgPower":50.0002326965332}
-var teampulseData;
-teampulseData = fakedata;
-// fetch('https://data.teampulse.ch/raam/informations')
-//   .then(function(response) {
-//     return response.json()
-//   })
-//   .then(function(body){
-//     teampulseData = fakedata;
-//   });
+
+var thirdApiFetch = {
+	init: function () {
+		this.teampulse();
+	},
+	teampulse: function () {
+		var fakedata = {
+			"contestant": "CYCLIST_002",
+			"latitude": 46.764446,
+			"longitude": 6.646111,
+			"numberMinutes": 30,
+			"avgSpeed": 8.780640602111816,
+			"avgCadence": 40.0,
+			"avgPower": 50.0002326965332
+		}
+		tools.writeJson("teampulse.json", fakedata);
+		// fetch('https://data.teampulse.ch/raam/informations')
+		//   .then(function(response) {
+		//     return response.json()
+		//   })
+		//   .then(function(body){
+		//     tools.writeJson("teampulse.json", body);
+		//   });
+	}
+};
+
+/*****************
+  Common tools
+******************/
+
+var tools = {
+	writeJson: function (filename, data) {
+		fs.writeFile(dataPath + filename, JSON.stringify(data), function (err) {
+			if (err) {
+				return console.log(err);
+			}
+		});
+	},
+	readJson: function (filename) {
+		return JSON.parse(fs.readFileSync(dataPath + filename, 'utf8'));
+	}
+};
 
 /*****************
   ROUTES
 ******************/
 
-app.get('/teampulse-data',cors(), function (req, res) {
-  res.json({ data: teampulseData });
+app.get('/teampulse-data', cors(), function (req, res) {
+	res.json(
+		tools.readJson("instagram.json")
+	);
 })
 
-app.get('/fbposts',cors(), function (req, res) {
-  res.json({ postsId: fbPostsId });
+app.get('/fbposts', cors(), function (req, res) {
+	res.json(
+		tools.readJson("facebook.json")
+	);
 })
 
-app.get('/flickrpics',cors(), function (req, res) {
-  res.json({ picsId: flickrPicsId });
+app.get('/flickrpics', cors(), function (req, res) {
+	res.json(
+		tools.readJson("flickr.json")
+	);
 })
 
-app.get('/instaposts',cors(), function (req, res) {
-  res.json({ instaPosts: instaPostsId });
+app.get('/instaposts', cors(), function (req, res) {
+	res.json(
+		tools.readJson("instagram.json")
+	);
 })
 
 app.get('/', function (req, res) {
-  res.send('Teampulse server!')
+	res.send('Teampulse server!')
 })
 
+// Init socialFetch
+socialFetch.init();
+thirdApiFetch.init();
+
 var port = process.env.PORT || 3999;
+
 app.listen(port, function () {
-  console.log('Example app listening on port '+port)
+	console.log('Teampulse app listening on port ' + port)
 })
