7dc13c5eb49c8f0654da7696694411116b8d196b
Oreonax <esok@mailoo.org>
2017-05-18 11:38:09 +0200

    Add cron task, favorite form with authentification + youtube, rewrite with es6 arrow,

--- app.js
+++ app.js
@@ -1,52 +1,93 @@
-var fs = require('fs');
-var dotenv = require('dotenv').config()
-var express = require('express')
-var app = express()
-var cors = require('cors')
-var Twitter = require('twitter');
-var graph = require('fbgraph');
-var fetch = require('node-fetch');
+const fs = require('fs');
+const { URL, URLSearchParams } = require('url');
+const cron = require('node-cron');
+const dotenv = require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
 
-var Flickr = require("flickrapi");
+const Twitter = require('twitter');
+const Flickr = require("flickrapi");
+const graph = require('fbgraph');
+const fetch = require('node-fetch');
 
-var corsOptions = {
+const bodyParser = require('body-parser');
+
+const urlencoded = bodyParser.urlencoded({
+	extended: true
+});
+
+const auth = require('http-auth');
+const basic = auth.basic({
+	realm: "Gestion du favori",
+	file: __dirname + "/users.htpasswd"
+});
+
+const authMiddleware = auth.connect(basic);
+
+const corsOptions = {
 	origin: 'https://mediacomem.github.io',
 	optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
 }
 
-var dataPath = "data/";
+const dataPath = __dirname + "/data/";
+
+const regex = {
+	youtube: /http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?/i,
+	flickr: /\/photos\/([0-9@a-z]*)\/albums\/([0-9]*)/i
+}
 
 var socialFetch = {
 
-	init: function () {
+	init() {
 		this.instagram();
 		this.facebook();
-		this.flickr();
 	},
 
 	/*****************
 		INSTAGRAM
 	******************/
 
-	instagram: function () {
+	instagram() {
 		fetch('https://api.instagram.com/v1/users/self/media/liked?access_token=' + process.env.INSTAGRAM_ACCESS_TOKEN)
-			.then(function (res) {
+			.then((res) => {
 				return res.json()
 			})
-			.then(function (body) {
-				return body.data.map(function (d) {
+			.then((body) => {
+				return body.data.map((d) => {
 					return {
 						id: d.id,
-						link: d.link
+						url: d.link
 					}
 				});
 			})
-			.then(function (posts) {
+			.then((posts) => {
 				tools.writeJson("instagram.json", posts);
 			})
-			.catch(function (err) {
+			.catch((err) => {
 				console.log(err);
 			});
+	},
+
+
+	/*****************
+  	FACEBOOK
+	******************/
+
+	facebook() {
+		graph.setAccessToken(process.env.FACEBOOK_ACCESS_TOKEN);
+
+		graph.get("teampulse.ch/feed?limit=5", (err, res) => {
+			if (err) return console.log(err);
+
+			var posts = res.data.map((d) => {
+				return {
+					id: d.id.split("_").pop()
+				}
+			});
+
+			tools.writeJson("facebook.json", posts);
+		});
 	},
 
 	/*****************
@@ -56,57 +97,59 @@ var socialFetch = {
 		api_key: process.env.FLICKR_API_KEY,
 		secret: process.env.FLICKR_SECRET
 	},
-	flickr: function () {
-		Flickr.tokenOnly(this.flickrOptions, function (error, flickr) {
+	flickr(url) {
+		var urlParams = url.match(regex.flickr);
+
+		Flickr.tokenOnly(this.flickrOptions, (error, flickr) => {
 			flickr.photosets.getPhotos({
-				photoset_id: "72157661572108305",
-				user_id: "135987724@N05",
+				user_id: urlParams[1],
+				photoset_id: urlParams[2],
 				page: 1,
 				per_page: 500
-			}, function (err, result) {
-				if (err) throw err;
+			}, (err, result) => {
+				if (err) return console.log(err);
 
-				var photos = result.photoset.photo.map(function (d) {
+				var photos = result.photoset.photo.map((d) => {
 					return {
 						url: "https://farm" + d.farm + ".staticflickr.com/" + d.server + "/" + d.id + "_" + d.secret + "_z.jpg"
 					}
 				});
 
-				tools.writeJson("flickr.json", photos);
+				tools.writeJson("favori.json", photos);
 			});
 		});
 	},
 
 	/*****************
-  FACEBOOK
+  	YOUTUBE
 	******************/
 
-	facebook: function () {
-		graph.setAccessToken(process.env.FACEBOOK_ACCESS_TOKEN);
+	youtube(url) {
+		var url = new URL(url);
+		var urlParams = url.searchParams;
 
-		graph.get("teampulse.ch/feed?limit=5", function (err, res) {
-			if (err) throw err;
+		var video = {
+			v: urlParams.get('v')
+		}
 
-			var posts = res.data.map(function (d) {
-				return {
-					id: d.id.split("_").pop()
-				}
-			});
+		if(urlParams.has('list')){
+			video.list = urlParams.get('list')
+		}
 
-			tools.writeJson("facebook.json", posts);
-		});
+		tools.writeJson("favori.json", [video]);
 	}
+
 }
 
 /*****************
   TEAMPULSE API
 ******************/
 
-var thirdApiFetch = {
-	init: function () {
+var thirdFetch = {
+	init() {
 		this.teampulse();
 	},
-	teampulse: function () {
+	teampulse() {
 		var fakedata = {
 			"contestant": "CYCLIST_002",
 			"latitude": 46.764446,
@@ -117,13 +160,16 @@ var thirdApiFetch = {
 			"avgPower": 50.0002326965332
 		}
 		tools.writeJson("teampulse.json", fakedata);
-		// fetch('https://data.teampulse.ch/raam/informations')
-		//   .then(function(response) {
-		//     return response.json()
-		//   })
-		//   .then(function(body){
-		//     tools.writeJson("teampulse.json", body);
-		//   });
+		/*fetch('https://data.teampulse.ch/raam/informations')
+			.then(function (response) {
+				return response.json()
+			})
+			.then(function (body) {
+				tools.writeJson("teampulse.json", body);
+			});
+		.catch(function (err) {
+			console.log(err);
+		});*/
 	}
 };
 
@@ -132,14 +178,12 @@ var thirdApiFetch = {
 ******************/
 
 var tools = {
-	writeJson: function (filename, data) {
-		fs.writeFile(dataPath + filename, JSON.stringify(data), function (err) {
-			if (err) {
-				return console.log(err);
-			}
+	writeJson(filename, data) {
+		fs.writeFile(dataPath + filename, JSON.stringify(data), (err) => {
+			if (err) return console.log(err);
 		});
 	},
-	readJson: function (filename) {
+	readJson(filename) {
 		return JSON.parse(fs.readFileSync(dataPath + filename, 'utf8'));
 	}
 };
@@ -148,40 +192,86 @@ var tools = {
   ROUTES
 ******************/
 
-app.get('/teampulse-data', cors(), function (req, res) {
+app.get('/', (req, res) => {
+	res.send('Teampulse server!')
+})
+
+app.get('/teampulse/data', cors(), (req, res) => {
 	res.json(
 		tools.readJson("instagram.json")
 	);
 })
 
-app.get('/fbposts', cors(), function (req, res) {
+app.get('/facebook/posts', cors(), (req, res) => {
 	res.json(
 		tools.readJson("facebook.json")
 	);
 })
 
-app.get('/flickrpics', cors(), function (req, res) {
-	res.json(
-		tools.readJson("flickr.json")
-	);
-})
-
-app.get('/instaposts', cors(), function (req, res) {
+app.get('/instagram/posts', cors(), (req, res) => {
 	res.json(
 		tools.readJson("instagram.json")
 	);
 })
 
-app.get('/', function (req, res) {
-	res.send('Teampulse server!')
+// Handle favori
+
+app.get('/favori', authMiddleware, urlencoded, (req, res) => {
+	var html =
+		`<p>Bonjour ${req.user}!</p>
+			<form action="/favori" method="post">
+               <label for="url">Entrez, s'il vous plaît, une URL d'un album flickr ou d'une vidéo youtube:</label>
+               <input type="url" name="url" placeholder="http://..." />
+               <br/>
+               <button type="submit">Envoyer</button>
+            </form>`;
+
+	res.send(html);
+});
+
+app.post('/favori', authMiddleware, urlencoded, (req, res) => {
+	var url = req.body.url;
+	var html = `🎉🎉🎉 <p>La section favori a été mise à jour avec : ${url} </p>`;
+
+	if(regex.flickr.test(url)){
+		socialFetch.flickr(url);
+	}
+	else if(regex.youtube.test(url)){
+		socialFetch.youtube(url);
+	}
+	else{
+		html = `<p>L'url indiquée est incorrecte</p><a href="/favori">Veuillez réessayer</a>`
+	}
+
+	res.send(html);
+});
+
+app.get('/favori/data', cors(), (req, res) => {
+	res.json(
+		tools.readJson("favori.json")
+	);
 })
 
 // Init socialFetch
 socialFetch.init();
-thirdApiFetch.init();
+thirdFetch.init();
 
 var port = process.env.PORT || 3999;
 
-app.listen(port, function () {
+app.listen(port, () => {
 	console.log('Teampulse app listening on port ' + port)
 })
+
+// Update social feeds every 5 minutes
+
+cron.schedule('*/5 * * * *', () => {
+	socialFetch.init();
+	console.log(new Date() + '- Social feeds updated');
+});
+
+// Update teampulse feed every minute
+
+cron.schedule('* * * * *', () => {
+	thirdFetch.init();
+	console.log(new Date() + '- Teampulse feed updated');
+});
